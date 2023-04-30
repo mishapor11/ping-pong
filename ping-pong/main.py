4300146760b0f0686c80784f3f1983bd8865faa0
mishiniy_profil <119784786+mishapor11@users.noreply.github.com>
2023-04-30 13:30:19 +0500

    1.1

--- ping-pong/main.py
+++ ping-pong/main.py
@@ -4,13 +4,13 @@ window = display.set_mode((700, 500))
 display.set_caption('ping-pong')  
 
 background = transform.scale(
-    image.load('es.jpg'),
+    image.load('ping-pong\\es.jpg'),
     (700, 500)
 ) 
 
 class GameSprite(sprite.Sprite):
     def __init__(self, img, speed, x, y):
-        self.img = transform.scale(image.load(img), (100,100))
+        self.img = transform.scale(image.load(img), (250,100))
         self.speed = speed
         self.rect = self.img.get_rect()
         self.rect.x = x
@@ -30,15 +30,25 @@ class Player(GameSprite):
         if keys_pressed[K_s] and self.rect.y < 405:
             self.rect.y += self.speed
 
+    def control2(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 0 :
+            self.rect.y -= self.speed    
+        if keys_pressed[K_DOWN] and self.rect.y < 405:
+            self.rect.y += self.speed
+
 clock = time.Clock()
 FPS = 60
 game = True
 
-player = Player('palka.png', 30, 10, 400)
+player = Player('ping-pong\\palka.png', 30, -50, 400)
+player2 = Player('ping-pong\\palka.png', 30, 525, 400)
 while game:
     window.blit(background,(0, 0))
     player.reset()
     player.control()
+    player2.reset()
+    player2.control2()
     for e in event.get():
         if e.type == QUIT:
             game = False
