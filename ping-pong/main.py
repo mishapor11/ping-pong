41015f3c9712f2acb451cc8aaadeb7518ad044f4
mishiniy_profil <119784786+mishapor11@users.noreply.github.com>
2023-05-13 16:47:46 +0500

    Update main.py

--- ping-pong/main.py
+++ ping-pong/main.py
@@ -9,9 +9,10 @@ background = transform.scale(
 ) 
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, img, speed, x, y, img_width, img_height):
+    def __init__(self, img, speed, x, y, img_width, img_height, pon):
         self.img = transform.scale(image.load(img), (img_width, img_height))
         self.speed = speed
+        self.pon = pon
         self.rect = self.img.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -19,8 +20,8 @@ class GameSprite(sprite.Sprite):
         window.blit(self.img, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def __init__(self, img, speed, x, y, img_width, img_height):
-            super().__init__(img, speed, x, y, img_width, img_height)
+    def __init__(self, img, speed, x, y, img_width, img_height, pon):
+            super().__init__(img, speed, x, y, img_width, img_height, pon)
 
 
     def control(self):
@@ -40,25 +41,21 @@ class Player(GameSprite):
 
 class Ball(GameSprite):
     def __init__(self, img, speed, x, y, speed_x, speed_y, img_width, img_height, pon):
-        super().__init__(img, speed, x, y, img_width, img_height)
+        super().__init__(img, speed, x, y, img_width, img_height, pon)
         self.speed_x = speed_x
         self.speed_y = speed_y
-        self.pon = pon
-        
 
     def kuda(self):
-        if self.pon == 1:
-            self.rect.x += self.speed_x
-            self.rect.y += self.speed_y
-
-        if sprite.collide_rect(ball, player2) and self.pon == 1:
-            self.pon = 2
-
-        if self.pon == 2:
-            self.rect.x -= self.speed_x
-            self.rect.y -= self.speed_y
         
-        
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if sprite.collide_rect(ball, player2):
+            self.speed_x *= -1
+            self.speed_y *= -1
+
+        if self.rect.y <= 0:
+            
 
 ball = 'ball.png'
 
@@ -66,9 +63,9 @@ clock = time.Clock()
 FPS = 60
 game = True
 
-player = Player('palka.png', 30, 50, 400, 20, 100)
-player2 = Player('palka.png', 30, 600, 400, 20, 100)
-ball = Ball(ball, 70, 350, 200, 3, 3, 50, 50, 1)
+player = Player('palka.png', 30, 50, 400, 20, 100, 0)
+player2 = Player('palka.png', 30, 600, 400, 20, 100, 0)
+ball = Ball(ball, 70, 400, 300, 3, 3, 50, 50, 0)
 while game:
     window.blit(background,(0, 0))
     player.reset()
